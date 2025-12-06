d87d2ea0ddc71032b4ae63ba1e574b1b7fca1eb6
Sayed Mosad <sayedmosad0022@gmail.com>
2025-12-07 00:17:19 +0200

    home

--- codecrefactos/lib/views/home.dart
+++ codecrefactos/lib/views/home.dart
@@ -2,12 +2,21 @@ import 'package:codecrefactos/widgets/custom_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
+import '../viewmodels/EmployeeModel.dart';
+import '../viewmodels/SalesData.dart';
 import '../widgets/appbar.dart';
+import '../widgets/employee_item.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,41 +25,218 @@ class Home extends StatelessWidget {
         preferredSize: Size.fromHeight(60),
         child: Appbar(),
       ),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 10.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            Gap(10.h),
-            Text('Dashboard',style: TextStyle(
-              fontSize: 30.sp,
-              fontWeight: FontWeight.bold,
-              color: Color(0xff0A0A0A),
-            ),),
-            Text('Welcome to the Company Management System',style: TextStyle(
-              fontSize: 16.sp,
-              fontWeight: FontWeight.bold,
-              color: Color(0xff717182),
-            ),),
-            Gap(10.h),
-            ////////////////////////////////////////////////
-            Row(
-          children: [
-            Expanded(child: CustomCard(toptext:' Total Employees', number: "48", imagepath: 'assets/employee.png', bottomtext: "+12% from last month")),
-            Gap(10.w),
-            Expanded(child: CustomCard(toptext:' Total Employees', number: "48", imagepath: 'assets/employee.png', bottomtext: "+12% from last month"))
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gap(10.h),
+              Text(
+                'Dashboard',
+                style: TextStyle(
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff0A0A0A),
+                ),
               ),
-            Gap(10.h),
-
-            Row(
-          children: [
-            Expanded(child: CustomCard(toptext:' Total Employees', number: "48", imagepath: 'assets/employee.png', bottomtext: "+12% from last month")),
-            Gap(10.w),
-            Expanded(child: CustomCard(toptext:' Total Employees', number: "48", imagepath: 'assets/employee.png', bottomtext: "+12% from last month"))
-          ],
+              Text(
+                'Welcome to the Company Management System',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff717182),
+                ),
               ),
-             ] )),
+              Gap(10.h),
+              ////////////////////////////////////////////////
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomCard(
+                      toptext: ' Total Employees',
+                      number: "48",
+                      imagepath: 'assets/employee.png',
+                      bottomtext: "+12% from last month",
+                    ),
+                  ),
+                  Gap(10.w),
+                  Expanded(
+                    child: CustomCard(
+                      toptext: ' Total Employees',
+                      number: "48",
+                      imagepath: 'assets/employee.png',
+                      bottomtext: "+12% from last month",
+                    ),
+                  ),
+                ],
+              ),
+              Gap(10.h),
+        
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomCard(
+                      toptext: ' Total Employees',
+                      number: "48",
+                      imagepath: 'assets/employee.png',
+                      bottomtext: "+12% from last month",
+                    ),
+                  ),
+                  Gap(10.w),
+                  Expanded(
+                    child: CustomCard(
+                      toptext: ' Total Employees',
+                      number: "48",
+                      imagepath: 'assets/employee.png',
+                      bottomtext: "+12% from last month",
+                    ),
+                  ),
+                ],
+              ),
+              Gap(10.h),
+              Container(
+                height: 285.h,
+                width: 420.w,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 12,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Monthly Sales & Purchases',
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff0A0A0A),
+                        ),
+                      ),
+                      Gap(10.h),
+                      Expanded(
+                        child: SfCartesianChart(
+                          primaryXAxis: CategoryAxis(
+                            labelStyle: TextStyle(fontSize: 12.sp),
+                            majorGridLines: MajorGridLines(width: 0),
+                          ),
+                          primaryYAxis: NumericAxis(
+                            minimum: 0,
+                            maximum: 140000,
+                            interval: 35000,
+                            labelStyle: TextStyle(fontSize: 12.sp),
+                          ),
+                          legend: Legend(
+                            isVisible: true,
+                            position: LegendPosition.bottom,
+                            overflowMode: LegendItemOverflowMode.wrap,
+                          ),
+                          tooltipBehavior: TooltipBehavior(enable: true),
+                          series: <CartesianSeries>[
+                            ColumnSeries<SalesData, String>(
+                              name: 'Sales',
+                              width: 0.5.w,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(6.r),
+                              ),
+                              dataSource: [
+                                SalesData(month: 'January', value: 65000),
+                                SalesData(month: 'February', value: 78000),
+                                SalesData(month: 'March', value: 120000),
+                              ],
+                              xValueMapper: (data, _) => data.month,
+                              yValueMapper: (data, _) => data.value,
+                              color: Color(0xFF4285F4),
+                            ),
+        
+                            ColumnSeries<SalesData, String>(
+                              name: 'Purchases',
+                              width: 0.5.w,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(6.r),
+                              ),
+                              dataSource: [
+                                SalesData(month: 'January', value: 35000),
+                                SalesData(month: 'February', value: 48000),
+                                SalesData(month: 'March', value: 78000),
+                              ],
+                              xValueMapper: (data, _) => data.month,
+                              yValueMapper: (data, _) => data.value,
+                              color: Color(0xFFFF9800),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ),
+              Gap(10),
+              Container(
+                height: 200.h,
+                width: 420.w,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 12,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      Text('Top Performing Employees',style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff0A0A0A),
+                      ),),
+                      Gap(6.h),
+                      EmployeeItem(
+                        employee: EmployeeModel(
+                          imagePath: "assets/rat1.png",
+                          name: "John Smith",
+                          salesText: "\$40,230 in sales",
+                          percent: 0.85,
+                          percentText: "85%",
+                        ),
+                      ),
+                      Gap(20),
+                      EmployeeItem(
+                        employee: EmployeeModel(
+                          imagePath: "assets/rat2.png",
+                          name: "Sarah Johnson",
+                          salesText: "\$38,540 in sales",
+                          percent: 0.58,
+                          percentText: "58%",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-  }}
+  }
+}
---
+++ codecrefactos/lib/widgets/employee_item.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:gap/gap.dart';
+import '../viewmodels/EmployeeModel.dart';
+
+class EmployeeItem extends StatelessWidget {
+  final EmployeeModel employee;
+
+  const EmployeeItem({super.key, required this.employee});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 45.w,
+          height: 45.h,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey.shade200,
+          ),
+          child: ClipOval(
+            child: Image.asset(
+              employee.imagePath,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+         Gap(12.w),
+
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                employee.name,
+                style:  TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Text(
+                employee.salesText,
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 14.sp,
+                ),
+              ),
+
+               Gap(6.h),
+
+              LinearPercentIndicator(
+                lineHeight: 10.h,
+                barRadius:  Radius.circular(10.r),
+                percent: employee.percent,
+                backgroundColor: Colors.grey.shade300,
+                linearGradient:  LinearGradient(
+                  colors: [
+                    Color(0xff3c9df4),
+                    Color(0xffa855f7),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+         Gap(10.h),
+
+        Text(
+          employee.percentText,
+          style:  TextStyle(
+            fontSize: 15.sp,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
